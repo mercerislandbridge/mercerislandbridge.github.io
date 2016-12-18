226301fa8f0dc09ca36edf2f382e2203d4ffd6e7
Mercercrest Bridge Club <mercerislandbridge@gmail.com>
2016-12-18 11:01:40 -0800

    Add date

--- _jekyll/_scripts/generate_blog.py
+++ _jekyll/_scripts/generate_blog.py
@@ -2,9 +2,11 @@ import re
 import sys
 import time
 title = time.strftime("%B %d, %Y")
+curr_time = time.strftime("%Y-%m-%d %H:%M:%S +0000", time.gmtime())
 print '---'
 print 'layout: post'
 print 'title:  "%s" Results' % title
+print 'date: %s' % curr_time
 print 'categories: results'
 print '---'
 for line in sys.stdin:
