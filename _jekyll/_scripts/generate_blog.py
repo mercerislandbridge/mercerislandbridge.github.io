e0d36c04e2233d6cd16dca4071e6a424326b20cb
Mercercrest Bridge Club <mercerislandbridge@gmail.com>
2016-12-21 08:56:19 -0800

    Fix script

--- _jekyll/_scripts/generate_blog.py
+++ _jekyll/_scripts/generate_blog.py
@@ -5,7 +5,7 @@ title = time.strftime("%B %d, %Y")
 curr_time = time.strftime("%Y-%m-%d %H:%M:%S +0000", time.gmtime())
 print '---'
 print 'layout: post'
-print 'title:  "%s" Results' % title
+print 'title:  "%s Results"' % title
 print 'date: %s' % curr_time
 print 'categories: results'
 print '---'
