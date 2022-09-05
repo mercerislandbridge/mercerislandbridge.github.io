0394b9c67bd0aeee795b18cb0278c5c5fe14140a
Mercercrest Bridge Club <mercerislandbridge@gmail.com>
2022-09-05 15:47:11 -0700

    Update print statements for Python3

--- _jekyll/_scripts/generate_blog.py
+++ _jekyll/_scripts/generate_blog.py
@@ -3,18 +3,18 @@ import sys
 import time
 title = time.strftime("%B %d, %Y")
 curr_time = time.strftime("%Y-%m-%d %H:%M:%S +0000", time.gmtime())
-print '---'
-print 'layout: post'
-print 'title:  "%s Results"' % title
-print 'date: %s' % curr_time
-print 'categories: results'
-print '---'
+print('---')
+print('layout: post')
+print('title:  "%s Results"' % title)
+print('date: %s' % curr_time)
+print('categories: results')
+print('---')
 count = 0;
 for line in sys.stdin:
     if count < 7:
         count += 1
     else:
-        print re.sub("(http\S*)", "[\g<0>](\g<0>)", line.strip())
+        print(re.sub("(http\S*)", "[\g<0>](\g<0>)", line.strip()))
 
 
 
